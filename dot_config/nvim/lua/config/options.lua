c9e69895cf494bcb887aceb051509a6203d17d02
Brahming Wu <brahmingwu@outlook.com>
2025-12-25 00:11:54 +0800

    Update Config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local opt = vim.opt
+opt.spelllang = { "en", "cjk" }
+
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
 
--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -7,4 +7,19 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "catppuccin/nvim",
+    opts = {
+      background = { -- :h background
+        light = "latte",
+        dark = "macchiato",
+      },
+      transparent_background = true,
+    },
+    float = {
+      transparent = true, -- enable transparent floating windows
+      solid = true, -- use solid styling for floating windows, see |winborder|
+    },
+    auto_integrations = true,
+  },
 }
