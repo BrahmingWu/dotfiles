9fe8e7cb692c91d39594987fde3be7b6757eac09
Brahming Wu <brahmingwu@outlook.com>
2026-04-03 23:04:19 +0800

    update editor ui config

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local modes = { "n", "i", "v", "s" }
+for _, mode in ipairs(modes) do
+  pcall(vim.keymap.del, mode, "<C-s>")
+end
+local map = LazyVim.safe_keymap_set
+-- vim.keymap.set("n", "<C-D-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+-- vim.keymap.set("n", "<C-D-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+-- vim.keymap.set("n", "<C-D-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+-- vim.keymap.set("n", "<C-D-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+
+
+
+map("n", "<C-D-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-D-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<C-D-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-D-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+Snacks.toggle.zoom():map("<C-D-CR>"):map("<C-w>m")
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -6,3 +6,82 @@ local opt = vim.opt
 opt.spelllang = { "en", "cjk" }
 vim.g.autoformat = false
 
+if vim.g.neovide then
+  vim.print(vim.g.neovide_version)
+  -- Put anything you want to happen only in Neovide here
+
+  -- vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  -- vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  -- vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  -- vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode" -- text below applies for VimScript
+  -- Allow clipboard copy paste in neovim
+  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+  -- vim.g.neovide_cursor_vfx_mode = "ripple"
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_floating_corner_radius = 0.5
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+  vim.g.neovide_input_macos_option_key_is_meta = "both"
+
+  vim.g.neovide_opacity = 0.70
+  vim.g.neovide_normal_opacity = 0.70
+
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  vim.g.neovide_theme = "auto"
+  vim.g.neovide_progress_bar_enabled = true
+  vim.g.neovide_progress_bar_height = 5.0
+  vim.g.neovide_progress_bar_animation_speed = 200.0
+  vim.g.neovide_progress_bar_hide_delay = 0.2
+
+  vim.g.neovide_highlight_matching_pair = true
+
+  local function set_ime(args)
+    if args.event:match("Enter$") then
+      vim.g.neovide_input_ime = true
+    else
+      vim.g.neovide_input_ime = false
+    end
+  end
+
+  local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime,
+  })
+
+  vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "[/\\?]",
+    callback = set_ime,
+  })
+
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_floating_blur_amount_x = 10.0
+  vim.g.neovide_floating_blur_amount_y = 10.0
+  vim.g.neovide_show_border = false
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<D-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<D-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+  vim.keymap.set("n", "<D-0>", function()
+    vim.g.neovide_scale_factor = 1.0
+  end)
+end
