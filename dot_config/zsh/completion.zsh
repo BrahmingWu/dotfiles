964260c7ca88a860440b04180c3f54147701d5a4
Brahming Wu <brahmingwu@outlook.com>
2026-05-06 14:53:29 +0800

    Remove legacy .zsh files
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- dot_config/zsh/completion.zsh
+++
@@ -1,7 +0,0 @@
-autoload -Uz compinit
-if [[ -n ${ZDOTDIR:-$HOME}/.zcompdump(#qN.m-1) ]]; then
-  compinit -C
-else
-  compinit
-fi
-
--- dot_config/zsh/plugins.zsh
+++
@@ -1,4 +0,0 @@
-eval "$(starship init zsh)"
-eval "$(zoxide init zsh)"
-eval "$(mise activate zsh)"
-# eval "$(zellij setup --generate-auto-start zsh)"
