22a8d5d2acaf9b4de1428124b42a2fb0aa37a94b
Brahming Wu <brahmingwu@outlook.com>
2026-05-19 13:32:51 +0800

    misc update

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -9,17 +9,24 @@ return {
   },
   {
     "catppuccin/nvim",
+    name = "catppuccin",
     opts = {
+      flavour = "auto", -- latte, frappe, macchiato, mocha
       background = { -- :h background
         light = "latte",
         dark = "macchiato",
       },
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.10, -- percentage of the shade to apply to the inactive window
+      },
       transparent_background = true,
+      float = {
+        transparent = true, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
+      auto_integrations = true,
     },
-    float = {
-      transparent = true, -- enable transparent floating windows
-      solid = true, -- use solid styling for floating windows, see |winborder|
-    },
-    auto_integrations = true,
   },
 }
