2d19550f79e8901cf2099e005f3ad5b7376db6cd
Brahming Wu <brahmingwu@outlook.com>
2026-05-30 14:54:50 +0800

    add lsp bin by nvim mason to PATH

--- dot_config/zsh/env.sh
+++ dot_config/zsh/env.sh
@@ -2,7 +2,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export _ZO_DATA_DIR="$HOME/Documents/AppData/zoxide/"
 export LANG=en_US.UTF-8
 
-export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.local/bin:$HOME/.local/share/nvim/mason/bin"
 
 # export https_proxy=http://127.0.0.1:20122 http_proxy=http://127.0.0.1:20122 all_proxy=socks5://127.0.0.1:20122
 
