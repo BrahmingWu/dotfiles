970b3e93ae07e86150fd83f250a35fe6e3ffcafd
Brahming Wu <brahmingwu@outlook.com>
2026-08-13 14:51:29 +0800

    dev tools update

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,14 @@ return {
     format_on_save = false,
     formatters_by_ft = {
       kdl = { "kdlfmt" },
+      http = { "kulala-fmt" },
+    },
+    formatters = {
+      ["kulala-fmt"] = {
+        env = {
+          KULALA_CORE_PATH = vim.fn.stdpath("data") .. "/kulala.nvim/bin/kulala-core",
+        },
+      },
     },
   },
 }
